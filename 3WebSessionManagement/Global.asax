5bca712ac6b4025ed03ef0093a5b321f0fc82409
triangle0105 <543443317@qq.com>
2017-04-13 16:33:13 +0800

    create

---
+++ 3WebSessionManagement/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_3WebSessionManagement.MvcApplication" Language="C#" %>
---
+++ 3WebSessionManagement/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_3WebSessionManagement.Startup))]
+namespace _3WebSessionManagement
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
