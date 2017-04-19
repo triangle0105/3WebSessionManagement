44379689447f9120f07abc8d19ec773626ab4e58
triangle0105 <543443317@qq.com>
2017-04-19 18:00:47 +0800

    20170419

---
+++ 23DesginMode/32Modes/Mode2_SimpleFactory.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23DesginMode._32Modes
+{
+    /// <summary>
+    /// 封装改变，既然要封装改变，自然也就要找到改变的代码，然后把改变的代码用类来封装
+    /// 简单工厂类, 负责 炒菜
+    /// </summary>
+    public class Mode2_SimpleFactory
+    {
+        public static Food CreateFood(string type)
+        {
+            Food food = null;
+            if (type.Equals("土豆肉丝"))
+            {
+                food = new ShreddedPorkWithPotatoes();
+            }
+            else if (type.Equals("西红柿炒蛋"))
+            {
+                food = new TomatoScrambledEggs();
+            }
+
+            return food;
+        }
+    }
+
+    /// <summary>
+    /// 自己做饭的情况
+    /// 没有简单工厂之前，客户想吃什么菜只能自己炒的
+    /// </summary>
+    public class Customer
+    {
+        /// <summary>
+        /// 烧菜方法
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static Food Cook(string type)
+        {
+            Food food = null;
+            // 客户A说：我想吃西红柿炒蛋怎么办？
+            // 客户B说：那你就自己烧啊
+            // 客户A说： 好吧，那就自己做吧
+            if (type.Equals("西红柿炒蛋"))
+            {
+                food = new TomatoScrambledEggs();
+            }
+                // 我又想吃土豆肉丝, 这个还是得自己做
+                // 我觉得自己做好累哦，如果能有人帮我做就好了？
+            else if (type.Equals("土豆肉丝"))
+            {
+                food = new ShreddedPorkWithPotatoes();
+            }
+            return food;
+        }
+
+        private static void Main1(string[] args)
+        {
+            // 客户想点一个西红柿炒蛋        
+            Food food1 = Mode2_SimpleFactory.CreateFood("西红柿炒蛋");
+            food1.Print();
+
+            // 客户想点一个土豆肉丝
+            Food food2 = Mode2_SimpleFactory.CreateFood("土豆肉丝");
+            food2.Print();
+
+            Console.Read();
+        }
+
+    }
+
+    /// <summary>
+    /// 菜抽象类
+    /// </summary>
+    public abstract class Food
+    {
+        // 输出点了什么菜
+        public abstract void Print();
+    }
+
+    /// <summary>
+    /// 西红柿炒鸡蛋这道菜
+    /// </summary>
+    public class TomatoScrambledEggs : Food
+    {
+        public override void Print()
+        {
+            Console.WriteLine("一份西红柿炒蛋！");
+        }
+    }
+
+    /// <summary>
+    /// 土豆肉丝这道菜
+    /// </summary>
+    public class ShreddedPorkWithPotatoes : Food
+    {
+        public override void Print()
+        {
+            Console.WriteLine("一份土豆肉丝");
+        }
+    }
+}
---
+++ 23DesginMode/32Modes/Mode3_FactoryMethod.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23DesginMode._32Modes
+{
+    public class Mode3_FactoryMethod
+    {
+    }
+
+    /// <summary>
+    /// 抽象工厂类
+    /// </summary>
+    public abstract class Creator
+    {
+        // 工厂方法
+        public abstract Food CreateFoddFactory();
+    }
+
+    /// <summary>
+    /// 西红柿炒蛋工厂类
+    /// </summary>
+    public class TomatoScrambledEggsFactory : Creator
+    {
+        /// <summary>
+        /// 负责创建西红柿炒蛋这道菜
+        /// </summary>
+        /// <returns></returns>
+        public override Food CreateFoddFactory()
+        {
+            return new TomatoScrambledEggs();
+        }
+    }
+
+    /// <summary>
+    /// 土豆肉丝工厂类
+    /// </summary>
+    public class ShreddedPorkWithPotatoesFactory : Creator
+    {
+        /// <summary>
+        /// 负责创建土豆肉丝这道菜
+        /// </summary>
+        /// <returns></returns>
+        public override Food CreateFoddFactory()
+        {
+            return new ShreddedPorkWithPotatoes();
+        }
+    }/// <summary>
+    /// 客户端调用
+    /// </summary>
+    class Client
+    {
+        static void Main1(string[] args)
+        {
+            // 初始化做菜的两个工厂（）
+            Creator shreddedPorkWithPotatoesFactory = new ShreddedPorkWithPotatoesFactory();
+            Creator tomatoScrambledEggsFactory = new TomatoScrambledEggsFactory();
+
+            // 开始做西红柿炒蛋
+            Food tomatoScrambleEggs = tomatoScrambledEggsFactory.CreateFoddFactory();
+            tomatoScrambleEggs.Print();
+
+            //开始做土豆肉丝
+            Food shreddedPorkWithPotatoes = shreddedPorkWithPotatoesFactory.CreateFoddFactory();
+            shreddedPorkWithPotatoes.Print();
+
+            Console.Read();
+        }
+    }  
+}
