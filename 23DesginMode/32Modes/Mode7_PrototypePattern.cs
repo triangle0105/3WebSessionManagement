9988fc0e5b4487b34b22fe956fb9979bb0c9d69a
triangle0105 <543443317@qq.com>
2017-04-20 17:38:55 +0800

    1

---
+++ 23DesginMode/32Modes/Mode7_PrototypePattern.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _23DesginMode._32Modes
+{
+    public class Mode7_PrototypePattern
+    {
+    }
+
+    class Client2
+    {
+        static void Main1(string[] args)
+        {
+            // 孙悟空 原型
+            MonkeyKingPrototype prototypeMonkeyKing = new ConcretePrototype("MonkeyKing");
+
+            // 变一个
+            MonkeyKingPrototype cloneMonkeyKing = prototypeMonkeyKing.Clone() as ConcretePrototype;
+            Console.WriteLine("Cloned1:\t" + cloneMonkeyKing.Id);
+
+            // 变两个
+            MonkeyKingPrototype cloneMonkeyKing2 = prototypeMonkeyKing.Clone() as ConcretePrototype;
+            Console.WriteLine("Cloned2:\t" + cloneMonkeyKing2.Id);
+            Console.ReadLine();
+        }
+    }
+    /// <summary>
+    /// 孙悟空原型
+    /// </summary>
+    public abstract class MonkeyKingPrototype
+    {
+        public string Id { get; set; }
+        public MonkeyKingPrototype(string id)
+        {
+            this.Id = id;
+        }
+
+        // 克隆方法，即孙大圣说“变”
+        public abstract MonkeyKingPrototype Clone();
+    }
+    /// <summary>
+    /// 创建具体原型
+    /// </summary>
+    public class ConcretePrototype : MonkeyKingPrototype
+    {
+        public ConcretePrototype(string id)
+            : base(id)
+        { }
+
+        /// <summary>
+        /// 浅拷贝
+        /// </summary>
+        /// <returns></returns>
+        public override MonkeyKingPrototype Clone()
+        {
+            // 调用MemberwiseClone方法实现的是浅拷贝，另外还有深拷贝
+            //浅拷贝是指当对象的字段值被拷贝时，字段引用的对象不会被拷贝。例如，如果一个对象有一个指向字符串的字段，并且我们对该对象做了一个浅拷贝，那么这两个对象将引用同一个字符串，而深拷贝是对对象实例中字段引用的对象也进行拷贝，如果一个对象有一个指向字符串的字段，并且我们对该对象进行了深拷贝的话，那么我们将创建一个对象和一个新的字符串，新的对象将引用新的字符串。也就是说，执行深拷贝创建的新对象和原来对象不会共享任何东西，改变一个对象对另外一个对象没有任何影响，而执行浅拷贝创建的新对象与原来对象共享成员，改变一个对象，另外一个对象的成员也会改变。
+            return (MonkeyKingPrototype)this.MemberwiseClone();
+        }
+    }
+}
